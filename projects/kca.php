05cd98dc6a93bae45c7723b4da21d8bdfe6e8539
Maria Szubski <MariaSzubski@gmail.com>
2017-04-24 10:05:29 -0400

    Move footer to template. Update Copyright year.

--- projects/kca.php
+++ projects/kca.php
@@ -117,26 +117,7 @@
 
 		</section>
 
-
-
-		<footer>
-			<h1>Get In Touch</h1>
-
-			<div id="contactSocial">
-				<ul>
-					<li><a href="https://www.linkedin.com/in/MariaSzubski" target="_blank"><div><img src="../img/soc_linkedin.svg" alt="LinkedIn"></div>LinkedIn</a></li>
-					<li><a href="https://github.com/MariaSzubski" target="_blank"><div><img src="../img/soc_github.svg" alt="GitHub"></div>GitHub</a></li>
-					<li><a href="https://www.hackerrank.com/MariaSzubski" target="_blank"><div><img src="../img/soc_hackerrank.svg" alt="Hackerrank"></div>Hackerrank</a></li>
-					<li><a href="https://twitter.com/MariaSzubski" target="_blank"><div><img src="../img/soc_twitter.svg" alt="Twitter"></div>Twitter</a></li>
-					<li><a href="mailto:mariaszubski@gmail.com" target="_blank"><div><img src="../img/soc_email.svg" alt="Email Me"></div>Email</a></li>
-				</ul>
-			</div>
-
-			<div id="copyright">
-				&copy;2016 Maria Szubski • MariaSzubski@gmail.com
-			</div>
-		</footer>
-
+		<?php require 'template/footer.inc.php'; ?>
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
