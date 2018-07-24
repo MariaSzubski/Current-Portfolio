cae76a6378dacea327a019fe91a3086658c89042
MariaSzubski <MariaSzubski@gmail.com>
2018-07-24 02:47:23 -0400

    Add Slick slideshow. Move testimonials to index.

---
+++ source/javascripts/site.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slideshow').slick({
+    dots: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    accessibility: true,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
